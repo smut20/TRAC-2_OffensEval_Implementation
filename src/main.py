da1b16a4a2fc69909c5bc12fb873f193eb527054
Seda Mut <sedamut@Sedas-MacBook-Air-6807.local>
2020-01-30 23:56:21 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,242 @@
+import spacy
+from spacymoji import Emoji
+
+import numpy as np
+import keras.backend as K
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional, SpatialDropout1D
+from keras import optimizers
+from keras.callbacks import EarlyStopping
+from keras.engine.topology import Layer, InputSpec
+from keras import initializers
+
+from keras.regularizers import L1L2
+from preprocess import preprocess_task3, create_data_task3, create_data_test_task3
+from utils import load_obj
+
+
+EMBEDDING_DIM = 300
+MAX_SEQUENCE_LENGTH = 50
+BATCH_SIZE = 250
+EPOCHS = 50
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+class AttentionWeightedAverage(Layer):
+    """
+    Computes a weighted average attention mechanism from:
+        Zhou, Peng, Wei Shi, Jun Tian, Zhenyu Qi, Bingchen Li, Hongwei Hao and Bo Xu.
+        “Attention-Based Bidirectional Long Short-Term Memory Networks for Relation Classification.”
+        ACL (2016). http://www.aclweb.org/anthology/P16-2034
+    How to use:
+    see: [BLOGPOST]
+    """
+
+    def __init__(self, return_attention=False, **kwargs):
+        self.init = initializers.get('uniform')
+        self.supports_masking = True
+        self.return_attention = return_attention
+        super(AttentionWeightedAverage, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(ndim=3)]
+        assert len(input_shape) == 3
+
+        self.w = self.add_weight(shape=(input_shape[2], 1),
+                                 name='{}_w'.format(self.name),
+                                 initializer=self.init)
+        self.trainable_weights = [self.w]
+        super(AttentionWeightedAverage, self).build(input_shape)
+
+    def call(self, h, mask=None):
+        h_shape = K.shape(h)
+        d_w, T = h_shape[0], h_shape[1]
+
+        logits = K.dot(h, self.w)  # w^T h
+        logits = K.reshape(logits, (d_w, T))
+        alpha = K.exp(logits - K.max(logits, axis=-1, keepdims=True))  # exp
+
+        # masked timesteps have zero weight
+        if mask is not None:
+            mask = K.cast(mask, K.floatx())
+            alpha = alpha * mask
+        alpha = alpha / K.sum(alpha, axis=1, keepdims=True)  # softmax
+        r = K.sum(h * K.expand_dims(alpha), axis=1)  # r = h*alpha^T
+        h_star = K.tanh(r)  # h^* = tanh(r)
+        if self.return_attention:
+            return [h_star, alpha]
+        return h_star
+
+    def get_output_shape_for(self, input_shape):
+        return self.compute_output_shape(input_shape)
+
+    def compute_output_shape(self, input_shape):
+        output_len = input_shape[2]
+        if self.return_attention:
+            return [(input_shape[0], output_len), (input_shape[0], input_shape[1])]
+        return (input_shape[0], output_len)
+
+    def compute_mask(self, input, input_mask=None):
+        if isinstance(input_mask, list):
+            return [None] * len(input_mask)
+        else:
+            return None
+
+
+def lstm_simple_binary_attent(embedding_matrix):
+    #opt = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
+
+    opt = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+    embedding_layer = Embedding(len(word_index) + 1,
+                                EMBEDDING_DIM,
+                                weights=[embedding_matrix],
+                                input_length=MAX_SEQUENCE_LENGTH,
+                                trainable=True)
+    d = 0.5
+    rnn_units = 30
+    model = Sequential()
+    model.add(embedding_layer)
+    model.add(SpatialDropout1D(d))
+    model.add(Bidirectional(LSTM(units=rnn_units, return_sequences=True, recurrent_regularizer=L1L2(l1=0.01, l2=0.01))))#, dropout=d, recurrent_dropout=rd)))
+    model.add(AttentionWeightedAverage())
+    model.add(Dropout(d))
+    model.add(Dense(3, activation='softmax'))
+
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy', f1])
+    return model
+
+if __name__ == '__main__':
+
+    CREATE_TRAIN_DATA = True
+    CREATE_TEST_DATA = True
+
+    #ORIGINAL FILES
+    train_file = "../data/offenseval-training-v1.tsv"
+    testing_path = "../data/test_set_taskc.tsv"
+    vocab_path = "../data/train_and_test_task3.tsv"
+
+
+    # TRAIN
+    new_path = "../data/offenseval-training-v1_task3.tsv"
+    word_index_pkl_path = "../data/word_index_train_test_task3.pkl"
+    embedding_matrix_path = "../data/embedding_matrix_task3.pkl"
+    emb_path = "/home/upf/Downloads/model_swm_300-6-10-low.w2v"
+    data_path = "../data/data_task3.pkl"
+    labels_path = "../data/labels_task3.pkl"
+
+    nlp = None
+    if CREATE_TRAIN_DATA:
+        preprocess_task3(train_file, new_path)
+        nlp = spacy.load('en')
+        emoji = Emoji(nlp)
+        nlp.add_pipe(emoji, first=True)
+        create_data_task3(new_path,
+                          vocab_path,
+                          word_index_pkl_path,
+                          embedding_matrix_path,
+                          emb_path,
+                          data_path,
+                          labels_path,
+                          nlp)
+
+
+    print("NLP Loaded")
+    word_index = load_obj(word_index_pkl_path)
+    print(len(word_index))
+    print("Word Index Loaded")
+    embedding_matrix = load_obj(embedding_matrix_path)
+    print(embedding_matrix.shape)
+    print("Embbeding Matrix Created")
+    data = load_obj(data_path)
+
+    #TEST
+    data_path = "../data/data_test_task3.pkl"
+    ids_test_path = "../data/ids_test_a_task3.pkl"
+    result_path = "../data/task_c_result.csv"
+
+
+    if CREATE_TEST_DATA:
+        if not nlp:
+            nlp = spacy.load('en')
+            emoji = Emoji(nlp)
+            nlp.add_pipe(emoji, first=True)
+
+        create_data_test_task3(data_path, ids_test_path, testing_path, word_index, nlp)
+
+    labels = load_obj(labels_path)
+
+    x_train=data
+    y_train=labels
+
+    print("Training Set Processed")
+
+    earlystop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='min')
+
+    callbacks_list = [earlystop]
+
+
+    # train the model
+    model = lstm_simple_binary_attent(embedding_matrix)
+    print(model.summary())
+    class_weight = {0: 4.,
+                    1: 1.,
+                    2: 1.5}
+
+    history = model.fit(x_train, y_train,
+              batch_size=BATCH_SIZE,
+              epochs=EPOCHS,
+              #validation_split=0.15,
+              callbacks=callbacks_list,
+              shuffle=True,
+              class_weight=class_weight)
+
+
+    label_test_dict = {0: "OAG",
+                       1: "CAG",
+                       2: "NAG"}
+
+    print("Testing....")
+
+    data_test = load_obj(data_path)
+    print(data_test.shape)
+    ids_test = load_obj(ids_test_path)
+
+    i = 0
+
+    out = open(result_path, "w")
+
+    while i < len(ids_test):
+        prediction = model.predict(np.array([data_test[i]]))
+        predicted_label = np.argmax(prediction[0])
+        out.write(ids_test[i] + "," + str(label_test_dict[predicted_label]) + "\n")
+        i += 1
+    out.close()
---
+++ src/preprocessTrac.py
@@ -0,0 +1,284 @@
+import re, csv, random, html
+from utils import save_obj
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+
+EMBEDDING_DIM = 300
+MAX_SEQUENCE_LENGTH = 50
+
+
+def create_data_task(train_file, vocab_path, word_index_pkl_path, embedding_matrix_path, emb_path, data_path,
+                      labels_path, word_index_path, nlp):
+    # WORD_INDEX
+    word_index = get_word_index(vocab_path, nlp)
+
+    out = open(word_index_path, "w", encoding='utf-8')
+    out.write("word" + "\t" + "index" + "\n")
+    for k in sorted(word_index):
+        out.write(k + "\t" + str(word_index[k]) + "\n")
+    out.close()
+    save_obj(word_index, word_index_pkl_path)
+
+    # embedding_matrix
+    embedding_matrix = get_pretrained_embedding(emb_path, word_index)
+    save_obj(embedding_matrix, embedding_matrix_path)
+
+    label_dict = {"OAG": 0,
+                  "CAG": 1,
+                  "NAG": 2}
+    data, labels = get_preprocessed_tweets(train_file, nlp, word_index, "subtask_a", label_dict)  # ?
+    save_obj(data, data_path)
+    save_obj(labels, labels_path)
+
+
+def prepocess_organizers_dataset(add_path, add_proc_path):
+    count = 0
+    total = 0
+
+    out = open(add_proc_path, "w", encoding='utf-8')
+    out.write("id\ttext\tsubtask_a\tsubtask_b\n")
+
+    with open(add_path, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            textColumn = row['Text'].replace("\n", " ")
+
+            total += 1
+            if len(textColumn.split(" ")) > 55:
+                continue
+            count += 1
+
+            textColumn = re.sub(r"http\S+", "URL", textColumn)
+            textColumn = re.sub('@[^\s]+', '@USER', textColumn)
+
+            outputText = row['ID'] + "\t" + textColumn + "\t" + row['Sub-task A'] + "\t" + row['Sub-task B'] + "\n"
+            out.write(outputText)
+
+    print("Preprocessing finished")
+    out.close()
+
+
+def prepocess_additional_dataset(add_path, add_proc_path):
+    count = 0
+    total = 0
+
+    out = open(add_proc_path, "w", encoding='utf-8')
+    with open(add_path, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            columnText = row['text'].replace("\n", " ")
+            total += 1
+            if columnText.startswith("RT"):
+                continue
+            if " RT " in columnText:
+                continue
+            if not len(columnText):
+                continue
+            count += 1
+
+            columnText = re.sub(r"http\S+", "URL", columnText)
+            columnText = re.sub('@[^\s]+', '@USER', columnText)
+
+            outputText = row['id'] + "\t" + columnText + "\t" + row['class'] + "\n"
+            out.write(outputText)
+
+    print(total)
+    print(count)
+    out.close()
+
+
+def preprocess_task3(train_file, new_path):
+    out = open(new_path, "w", encoding="utf-8")
+    original_train = []
+    duplicated_train = []
+    fd = open(train_file, "r", encoding="utf-8")
+    read = csv.DictReader(fd, dialect="excel-tab")
+    for row in read:
+        nl = row["id"] + "\t" + row["text"] + "\t" + row["subtask_a"] + "\t" + row["subtask_b"] + "\t"
+        original_train.append(nl)
+
+    random.shuffle(original_train)
+
+    test_count = int(len(original_train) * 0.15)
+    print("test_count: " + str(test_count))
+    print("duplicated_train: " + str(len(duplicated_train)))
+
+    test_data = original_train[:test_count]
+    original_train = original_train[test_count:]
+
+    perfect_duplicated_data = []
+
+    print("test_data: " + str(len(test_data)))
+    print("original_train: " + str(len(original_train)))
+
+    for l in duplicated_train:
+        if not l in test_data:
+            perfect_duplicated_data.append(l)
+
+    print("perfect_duplicated_data: " + str(len(perfect_duplicated_data)))
+
+    original_train = original_train + perfect_duplicated_data + perfect_duplicated_data + perfect_duplicated_data
+
+    random.shuffle(original_train)
+    random.shuffle(original_train)
+
+    original_train = test_data + original_train
+
+    out.write(
+        "id" + "\t" + "text" + "\t" + "subtask_a" + "\t" + "subtask_b" + "\n")  # here I need to delete the last subtask
+
+    for nl in original_train:
+        out.write(nl + "\n")
+
+    out.close()
+    fd.close()
+
+
+def get_pretrained_embedding(emb_path, word_index):
+    embeddings_index = {}
+    f = open(emb_path, encoding="utf-8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+
+    embedding_matrix = np.random.uniform(-0.8, 0.8, (len(word_index) + 1, EMBEDDING_DIM))
+
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+
+def clean_tweet(tweet_text):
+    tweet_text = tweet_text.replace('—', " ")  # .replace("'", "’")
+    tweet_text = ' '.join(tweet_text.split())
+    return tweet_text.strip()
+
+
+def text_processing(nlp, text):
+    tweet_text = clean_tweet(text)
+
+    tweet_text = tweet_text.replace("'", "&&&").replace("’", "&&&").replace("-", "&#&")
+
+    doc = nlp(tweet_text)
+    print(text)
+
+    for token in doc:
+        print(str(token).replace("&&&", "'").replace("&#&", "-"))
+
+
+def replace_tweet(tweet_text):
+    tweet_text = html.unescape(tweet_text)
+
+    return tweet_text.replace("'", "QUOTE_SYMBOL").replace("‘", "QUOTE_SYMBOL").replace("’", "QUOTE_SYMBOL").replace(
+        "-", "HYPH_SYMBOL").replace(";", " ").replace("#", "HASHTAG_SYMBOL")
+
+
+def unreplace_tweet(tweet_text):
+    return tweet_text.replace("QUOTE_SYMBOL", "'").replace("HYPH_SYMBOL", "-").replace("HASHTAG_SYMBOL", "#").replace(
+        "EMOJI_SYMBOL", "#&").lower()
+
+
+def get_preprocessed_labels(path, label_type="subtask_a", label_dict={"OFF": 1}):  # Here I changed "OFF" to
+    fd = open(path, "r", encoding="utf-8")
+    read = csv.DictReader(fd, dialect="excel-tab")
+    labels = []
+
+    for row in read:
+        label = label_dict.get(row[label_type], 0)
+        labels.append(label)
+
+    return labels
+
+
+def get_word_index(path, nlp):
+    fd = open(path, "r", encoding="utf-8")
+    read = csv.DictReader(fd, dialect="excel-tab")
+    word_set = set()
+    i = 0
+    for row in read:
+        i += 1
+        tweet_text = clean_tweet(row["tweet"])
+        tweet_text = replace_tweet(tweet_text)
+        doc = nlp(tweet_text)
+        for token in doc:
+            word = unreplace_tweet(str(token))
+            word_set.add(word)
+
+    word_index = {}
+    i = 0
+    for tok in word_set:
+        i += 1
+        word_index[tok] = i
+
+    fd.close()
+    return word_index
+
+
+def get_word_index_from_file(path):
+    fd = open(path, "r")
+
+    read = csv.DictReader(fd, dialect="excel-tab")
+    word_index = {}
+    for row in read:
+        word_index[row["word"]] = int(row["index"])
+    fd.close()
+    return word_index
+
+
+def get_preprocessed_tweets(path, nlp, word_index, label_type="subtask_a", label_dict={"OFF": 1}, max_instances=0):
+    print("Processing file " + path)
+
+    fd = open(path, "r", encoding="utf-8")
+    read = csv.DictReader(fd, dialect="excel-tab")
+    texts = []
+    labels = []
+    i = 0
+    for row in read:
+        sent = []
+        i += 1
+        tweet_text = clean_tweet(row["text"])
+        if "test" in label_type:
+            label = row["id"]
+        else:
+            label = label_dict.get(row[label_type], 0)
+        labels.append(label)
+        tweet_text = replace_tweet(tweet_text)
+
+        doc = nlp(tweet_text)
+
+        for token in doc:
+            word = unreplace_tweet(str(token))
+            sent.append(word)
+
+        texts.append(sent)
+        if i == max_instances:
+            break
+
+    sequences = []
+    for sent in texts:
+        seq = []
+        for word in sent:
+            i = word_index.get(word, 0)
+            if i:
+                seq.append(i)
+        sequences.append(seq)
+
+    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    if not "test" in label_type:
+        labels = to_categorical(np.asarray(labels))
+    fd.close()
+    return data, labels
+
+
+def create_test_data(data_path, ids_test_path, testing_path, word_index, nlp):
+    data_test, ids_test = get_preprocessed_tweets(testing_path, nlp, word_index, "test")
+    save_obj(data_test, data_path)
+    save_obj(ids_test, ids_test_path)
